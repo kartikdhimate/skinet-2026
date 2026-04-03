a9bb31e9499bd5691ec9a238e93118e1df06eda3
Kartik Dhimate <kartik.dhimate@tietoevry.com>
2026-04-03 13:43:16 +0300

    feat(cart): implement cart functionality with Redis integration; add CartController, CartService, and related models

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,9 @@
 using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,17 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddCors();
+builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
+{
+    var connectionString =builder.Configuration.GetConnectionString("Redis");
+    if(string.IsNullOrWhiteSpace(connectionString))
+    {
+        throw new Exception("Redis connection string is not configured.");
+    }
+    var configuration = ConfigurationOptions.Parse(connectionString, true);
+    return ConnectionMultiplexer.Connect(configuration);
+});
+builder.Services.AddSingleton<ICartService, CartService>();
 
 var app = builder.Build();
 
