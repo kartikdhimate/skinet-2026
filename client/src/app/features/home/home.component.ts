baa8fa56f15b641c312419952bc4c2fdcc5d1512
Kartik Dhimate <kartik.dhimate@tietoevry.com>
2026-04-21 18:47:25 +0300

    refactor: remove unused imports from app, register, home, and shop components

--- client/src/app/features/home/home.component.ts
+++ client/src/app/features/home/home.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [
-    MatButton,
     RouterLink
-  ],
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
