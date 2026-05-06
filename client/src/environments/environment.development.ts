f726ab7b36f6694a559b2b7f3116af56ba99e0ed
Kartik Dhimate <kartik.dhimate@tietoevry.com>
2026-05-06 17:03:01 +0300

    chore: update configuration files to use placeholder values for sensitive information

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
     hubUrl: 'https://localhost:5001/hub/notifications',
-    stripePublicKey: 'pk_test_51TJbzwB76eOyz1oV66RVTh26zWA9VoFH568VK5UMxOXfVMtTeAkuV5MrJICzZHL8Io8e2RNux4yDw2BtmpiKALSU00BoKDQs9b'
+    stripePublicKey: '<YOUR_STRIPE_PUBLIC_KEY>'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: 'pk_test_51TJbzwB76eOyz1oV66RVTh26zWA9VoFH568VK5UMxOXfVMtTeAkuV5MrJICzZHL8Io8e2RNux4yDw2BtmpiKALSU00BoKDQs9b'
+    stripePublicKey: '<YOUR_STRIPE_PUBLIC_KEY>'
 };
